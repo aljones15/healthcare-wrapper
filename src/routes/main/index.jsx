83789b21d078c5bfe614efdd7abc2577181bfc34
andrewjones <aljones15@gmail.com>
2018-11-18 15:45:16 -0500

    bread crumbs added

--- src/routes/main/index.jsx
+++ src/routes/main/index.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
-
+import { Select } from '@blueprintjs/select';
+import { Breadcrumbs, Breadcrumb, Icon } from "@blueprintjs/core";
 
 export class Main extends React.PureComponent {
   constructor(props) { 
-    super(props)
+    super(props);
+  }
+  getBread() {
+      const { location : _loc } = this.props;
+      const {pathname = "/" } = _loc;
+      const text = pathname.toUpperCase();
+      return [
+        { href: pathname, icon: "folder-close", text },
+      ];
   }
   render() {
-    return(<div id="main">MAIN</div>);
+    console.log('main props ', this.props);
+    return(
+      <div id="main">
+        <Breadcrumbs items={this.getBread()} />
+      </div>
+    );
   }
 }
 

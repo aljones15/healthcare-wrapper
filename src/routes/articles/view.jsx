b60934958f44aff5c421f44e977cfd78a05bce23
andrewjones <aljones15@gmail.com>
2018-11-27 15:18:22 -0500

    HTML Table

--- src/routes/articles/view.jsx
+++ src/routes/articles/view.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { HTMLTable } from '@blueprintjs/core';
 
 export function View(props) {
-  console.log("Article view props ", props);
+  console.log('View Articles ', props);
+  const articles = props.data ? props.data.articles : [];
+  const renderRows = () => articles.map(article => (
+    <tr key={article.path}><td>{article.title}</td></tr>));
   return (
-    <div>Articles</div>
+    <HTMLTable bordered={false} interactive={true}>
+      <thead>
+        <tr>
+          <th>Title</th>
+        </tr>
+      </thead>
+      <tbody>
+        {renderRows()}
+      </tbody>
+    </HTMLTable>
   );
 }
 
--- src/routes/header/index.js
+++ src/routes/header/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Select } from '@blueprintjs/select';
 import { Link } from 'react-router-dom';
-import { MenuItem, Button, Breadcrumbs, Breadcrumb, Icon, Navbar } from "@blueprintjs/core";
+import { MenuItem, Button, Icon, Navbar } from "@blueprintjs/core";
 const { Group } = Navbar;
 
 const selectOptions = [
@@ -17,19 +17,6 @@ export class RouterHeader extends React.PureComponent {
   constructor(props) { 
     super(props);
   }
-  getBread() {
-      const { location : _loc } = this.props;
-      const {pathname = "/" } = _loc;
-      const text = pathname.toUpperCase();
-      return [
-        { href: pathname, icon: "star", text },
-      ];
-  }
-  renderBread({text, icon, ...rest}) {
-      return(
-        <Breadcrumb {...rest}><Icon icon={icon} /> <span>{text}</span></Breadcrumb>
-      );
-  }
   renderOption({title, icon, path}){
     const { history } = this.props;
     return(<MenuItem key={title} text={title} onClick={() => history.push(path)} />);
@@ -41,10 +28,6 @@ export class RouterHeader extends React.PureComponent {
     return(
       <Navbar id="router-header">
         <Group>
-          <Navbar.Heading>
-            <Breadcrumbs currentBreadcrumbRenderer={this.renderBread} items={this.getBread()} />
-          </Navbar.Heading>
-          <Navbar.Divider />
           <Link to='/'>
             <Icon icon="home"/>
             <span>{' '}Home</span>
